5ee6df7b9dc8baae78c5c9f8fb4083443b696fa7
Mohammad Desai <P16219823@LEC-ADMIN.dmu.ac.uk>
2020-02-04 13:47:46 +0000

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+const hello = document.getElementById("hello");
+
+//uncomment below to see what getElementById 'got':
+//console.log(hello);
+
+function greeting() {
+  alert("Hello!");
+  hello.textContent = "Goodbye";
+}
+
+hello.addEventListener("click", greeting);
+
+/*
+ for textContent vs innerHTML vs innerText see:
+ https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent#Differences_from_innerText
+*/
